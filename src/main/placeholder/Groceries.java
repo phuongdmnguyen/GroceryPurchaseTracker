f95dc604ce037ee1b9a3ed69b38c37fb8c84b69d
Phuong Nguyen <pnguyen6@students.cs.ubc.ca>
2019-09-15 20:48:52 -0700

    add user input aspect

---
+++ src/main/placeholder/Groceries.java
@@ -0,0 +1,48 @@
+package placeholder;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Groceries {
+    private ArrayList<Item> groceries;
+    private Scanner scanner;
+
+    public Groceries() {
+        groceries = new ArrayList<Item>();
+        scanner = new Scanner(System.in);
+        processGroceries();
+    }
+
+    private void processGroceries() {
+        String iname = "";
+
+        while (true) {
+            System.out.println("Please input your item's name or quit");
+            iname = scanner.nextLine();
+
+            Item item = new Item(iname);
+
+            if (iname.equals("quit")) {
+                break;
+            }
+
+            System.out.println(iname);
+        }
+
+        System.out.println("end here!");
+    }
+
+
+    private int getTotal(ArrayList<Item> groceries) {
+        int sum = 0;
+        for (Item i : groceries) {
+            sum += i.cost;
+        }
+        return sum;
+    }
+
+    public static void main(String[]args) {
+        processGroceries();
+        getTotal();
+    }
+}
